7bfb125d6c705d3a73cfd89d09b660bc30635709
cotalora_ <67478427+cotalora@users.noreply.github.com>
2022-07-07 23:12:02 -0500

    Definición de estructura

---
+++ src/components/Layouts/LeftMenu.component/LeftMenu.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./LeftMenu.css"
+
+export class LeftMenu extends Component {
+    render() {
+        return (
+            <div className="left-menu-container">
+                <h2>LEFT MENU</h2>
+            </div>
+        )
+    }
+}
---
+++ src/components/Layouts/Topbar.component/Topbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Topbar.css"
+
+export class Topbar extends Component {
+    render() {
+        return (
+            <header className="top-bar-container">
+                <h2>TOPBAR</h2>
+            </header>
+        )
+    }
+}
